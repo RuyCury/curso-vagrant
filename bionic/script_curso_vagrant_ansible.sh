f2f444dc4c7e3b89d694819df92d5b43541c6b9f
Ruy Cury <ruy.cury@gmail.com>
2019-06-04 12:26:31 -0300

    Versão final do curso de vagrant

--- bionic/script_curso_vagrant_ansible.sh
+++ bionic/script_curso_vagrant_ansible.sh
@@ -2,7 +2,7 @@
 sudo apt-get update && \
 sudo apt-get install -y software-properties-common && \
 sudo apt-add-repository --yes --update ppa:ansible/ansible && \
-sudo apt-get install -y ansible aptitude && \
+sudo apt-get install -y ansible && \
 sudo cp /vagrant/id_bionic /home/vagrant && \
 sudo chmod 600 /home/vagrant/id_bionic && \
 sudo chown vagrant:vagrant /home/vagrant/id_bionic && \
